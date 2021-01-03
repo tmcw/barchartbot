67053ec589941bf6ffd841523dca51f0c7078dbe
Tom MacWright <tom@macwright.org>
2021-01-02 21:22:42 -0800

    Move chart code into reply code

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,108 @@
 const crypto = require("crypto");
 const request = require("request-promise");
+import { registerFont } from "canvas";
+import * as vega from "vega";
+import numeral from "numeral";
+import path from "path";
 
 const TWITTER_API = "https://api.twitter.com/1.1";
 const TWITTER_USERNAME = "@barchartbot";
 const GITHUB_URL = "https://github.com/alexluong/gimmedadjoke";
+
+registerFont(path.resolve("./public/Inter-Medium.ttf"), {
+  family: "Inter",
+});
+
+function spec(data) {
+  return {
+    $schema: "https://vega.github.io/schema/vega/v5.json",
+    description: "",
+    width: 800,
+    height: 400,
+    padding: 5,
+
+    config: {
+      background: "#ffffff",
+    },
+
+    data: [
+      {
+        name: "table",
+        values: data,
+      },
+    ],
+
+    scales: [
+      {
+        name: "yscale",
+        type: "band",
+        domain: { data: "table", field: "category" },
+        range: "height",
+        padding: 0.05,
+        round: true,
+      },
+      {
+        name: "xscale",
+        domain: { data: "table", field: "amount" },
+        nice: true,
+        range: "width",
+      },
+    ],
+
+    axes: [
+      {
+        orient: "bottom",
+        scale: "xscale",
+        labelFont: "Inter",
+        labelFontSize: 14,
+      },
+      {
+        orient: "left",
+        scale: "yscale",
+        labelFont: "Inter",
+        labelFontSize: 14,
+      },
+    ],
+
+    marks: [
+      {
+        type: "rect",
+        from: { data: "table" },
+        encode: {
+          enter: {
+            y: { scale: "yscale", field: "category" },
+            height: { scale: "yscale", band: 1 },
+            x: { scale: "xscale", field: "amount" },
+            x2: { scale: "xscale", value: 0 },
+            fill: { value: "steelblue" },
+          },
+        },
+      },
+    ],
+  };
+}
+
+function parseTweet(txt) {
+  const withoutUsername = txt.replace("@barchartbot", "");
+  const lines = withoutUsername
+    .split("\n")
+    .map((l) => l.trim())
+    .filter(Boolean);
+  const values = lines
+    .map((line) => {
+      const parts = line.split(":");
+      if (parts.length !== 2) return;
+      const category = parts[0];
+      const amount = numeral(parts[1]).value();
+      if (typeof amount !== "number") return;
+      return {
+        category,
+        amount,
+      };
+    })
+    .filter(Boolean);
+  return values;
+}
 
 function createCrcResponseToken(crcToken) {
   const hmac = crypto
@@ -39,8 +138,15 @@ function postHandler(req, res) {
 
   const tweet = body.tweet_create_events[0];
 
-  // Check if we're mentioned
   if (tweet.text.toLowerCase().includes(TWITTER_USERNAME)) {
+    const view = new vega.View(vega.parse(spec(parseTweet(tweet)), {}), {
+      renderer: "canvas",
+    }).finalize();
+
+    view.toCanvas(1).then((canvas) => {
+      canvas.createPNGStream().pipe(res);
+    });
+
     request
       .post({
         url: `${TWITTER_API}/statuses/update.json`,
